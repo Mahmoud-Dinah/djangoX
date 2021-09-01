b54957024bf7822fbe726eb2e0269930f3509c24
mahmoud-dinah <mahmood.dinah2@gmail.com>
2021-09-01 22:05:07 +0300

    added templates and fisnish the lab

--- snacks/models.py
+++ snacks/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.urls import reverse
+
 # Create your models here.
 
 class Snack(models.Model):
@@ -7,6 +9,10 @@ class Snack(models.Model):
     description = models.TextField(default='')
     purchaser = models.ForeignKey(get_user_model(),related_name='snacks',on_delete=models.CASCADE)
 
+    def get_absolute_url(self):
+        return reverse('snack_detail', args=[str(self.id)])
+
+  
     def __str__(self):
         return self.name
 
